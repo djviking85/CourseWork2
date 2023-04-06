8d106b269b39f260cf0e7399711b7b92b499669e
Viking <djviking@yandex.ru>
2023-04-06 16:41:42 +0300

    делаем курсовую

---
+++ src/main/java/pro/sky/exam/ExamQuestionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionApplication.class, args);
+	}
+
+}
