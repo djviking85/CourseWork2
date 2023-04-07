afdb187a6e03016fb80f771b749ffd3fa789471a
Viking <djviking@yandex.ru>
2023-04-07 19:03:59 +0300

    делаем курсовую

--- src/main/java/pro/sky/exam/controller/JavaController.java
+++ src/main/java/pro/sky/exam/controller/JavaController.java
@@ -1,10 +1,7 @@
 package pro.sky.exam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.sky.exam.service.JavaQuestionServiceImpl;
 
 @RestController
@@ -47,5 +44,6 @@ public class JavaController {
 //    public String hello3() {
 //        return "Добро пожаловать в удаление вопросов!";
 //    }
+//    @PathVariable - почитать для эмоунт
 
 }
